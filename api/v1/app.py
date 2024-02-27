add0f2bc44c2521631b7044f610fc343f5411b9d
Apples4 <nhlanhla.mavuka@gmail.com>
2024-02-27 12:40:53 +0200

    edditedapp.py again

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 import os
 from models import storage
 from flask import Flask, jsonify
-from api.v1.views import app_views
+from api.v1.views import *
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,7 +14,6 @@ port = os.environ.get('HBNB_API_PORT', '5000')
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
@@ -27,13 +26,6 @@ def close(cls):
 def not_found(e):
     """error 404 page"""
     return jsonify({"error": "Not found"}), 404
-
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone - RESTful API',
-    'description': 'This is an api,\
-    all the documentation will be shown below',
-    'uiversion': 3}
 
 
 if __name__ == '__main__':
