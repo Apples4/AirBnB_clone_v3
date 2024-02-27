cadf68956683655120a784289e167b8261c9afe3
Apples4 <nhlanhla.mavuka@gmail.com>
2024-02-27 04:33:39 +0200

    adding chances to app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,28 +2,33 @@
 """ app.py file """
 import os
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask_cors import CORS
+"""from flasgger import Swagger"""
 
 app = Flask(__name__)
-
-host = os.environ.get('HBNB_API_HOST', 0.0.0.0)
-port = os.environ.get('HBNB_API_PORT', 5000)
+"""swagger = swagger(app)"""
+db = os.environ.get('HBNB_TYPE_STORAGE', 'json_file')
+host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+port = os.environ.get('HBNB_API_PORT', '5000')
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
-
 @app.teardown_appcontext
-def closedb():
-    '''function to close db'''
-    return storage.close()
+def close(cls):
+    """close"""
+    storage.close()
+
 
 @app.errorhandler(404)
-def pagerr(err):
-    ''' function handles error '''
+def not_found(e):
+    """error 404"""
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    """Initialize api"""
     app.run(host=host, port=int(port), threaded=True)
