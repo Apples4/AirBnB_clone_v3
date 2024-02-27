72491289896112a93dc9483155277dba0e8cacd3
Apples4 <nhlanhla.mavuka@gmail.com>
2024-02-27 12:23:20 +0200

    edditedapp.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,6 @@ from models import storage
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from flask_cors import CORS
-"""from flasgger import Swagger"""
 
 app = Flask(__name__)
 """swagger = swagger(app)"""
@@ -15,18 +14,26 @@ port = os.environ.get('HBNB_API_PORT', '5000')
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
 def close(cls):
-    """close"""
+    """close database"""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(e):
-    """error 404"""
+    """error 404 page"""
     return jsonify({"error": "Not found"}), 404
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'This is an api,\
+    all the documentation will be shown below',
+    'uiversion': 3}
 
 
 if __name__ == '__main__':
